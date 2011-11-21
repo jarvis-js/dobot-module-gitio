12dbf1739a1c25abb4b03f3b53e8bfb226310142
Alex Whitman <alex@alexwhitman.com>
2011-11-21 17:02:02 +0000

    Add git.io module

---
+++ README.md
@@ -0,0 +1,6 @@
+# Git.io
+
+Create GitHub short URLs using [http://git.io](http://git.io)
+
+	> shorten <github_url>
+	> shorten <github_url> with <code>
---
+++ index.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var querystring = require('querystring');
+
+module.exports = function(bot, module) {
+
+	module.addCommand(/^(?:gitio|shorten) (https?:\/\/(?:gist\.)?github\.com\/[^ ]+)(?: (?:using|with) ([^\\\/ ]+))?$/i, function(request, url, code) {
+		var data = { url: url };
+		if (code) {
+			data.code = code;
+		}
+		var stringData = querystring.stringify(data);
+		var options = {
+			host: 'git.io',
+			port: 80,
+			path: '/',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': stringData.length
+			}
+		};
+		var req = http.request(options, function(res) {
+			if (res.statusCode === 201) {
+				request.reply = res.headers.location
+				bot.respond(request);
+			}
+			else if (res.statusCode === 422) {
+				var body = '';
+				res.on('data', function(chunk) {
+					body += chunk;
+				});
+				res.on('end', function() {
+					request.reply = body;
+				});
+				bot.respond(request);
+			}
+		});
+		req.on('error', function(error) {
+			request.reply = 'Unable to create a short url.';
+			bot.respond(request);
+		});
+		
+		req.write(stringData + "\n");
+		req.end();
+	});
+
+};
